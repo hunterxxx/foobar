09e2c7724e89aae89b7c43af03f6cbf0298ca198
Chinmay <cppingale@gmail.com>
2018-08-31 14:01:52 +0200

    pushing events div with layouts and handbars

--- src/components/gulpfile.js
+++ src/components/gulpfile.js
@@ -2,28 +2,30 @@ var gulp = require('gulp'); //add package gulp to gulp variable
 var cssClean = require('gulp-clean-css');
 var sass = require('gulp-sass');
 
-var srcFiles = ['event_index.html', 'css/style.css', 'js/script.js']
+var srcFiles = ['event_index.html', 'scss/events.scss', 'js/script.js']
 
-gulp.task('default', function(){		//can use gulp.start
-	//html
-	gulp.watch(['event_index.html', 'js/script.js', 'css/style.css'], ['copy']);
+gulp.task('default', function () {		//can use gulp.start
 
 	//css
 	gulp.watch('scss/*.scss', ['styles']);	//first compile and then minify
+
+	//html
+	gulp.watch(['event_index.html', 'js/events.js', 'images/*', 'css/events.css'], ['copy']);
 });
 
 //copy html js
-gulp.task('copy', function(){	
-	gulp.src(['event_index.html', 'js/script.js'])
+gulp.task('copy', function () {
+	gulp.src(['event_index.html', 'js/events.js', 'images/*', 'css/events.css'])
 		.pipe(gulp.dest('dist/'));
 	console.log('copied HTML and JS');
 });
 
 //compile scss
-gulp.task('styles', function(){
-	gulp.src('scss/style.scss')
+gulp.task('styles', function () {
+	gulp.src('scss/events.scss')
 		.pipe(sass())
 		.pipe(cssClean())
+		.pipe(gulp.dest('css/'))
 		.pipe(gulp.dest('dist/'));
 	console.log("compiled and cleaned scss")
 });
