b457bf3c6e672c8edcbee392950056e1a18ade8f
Duke Ovie <dukeovie09@gmail.com>
2018-09-03 16:31:00 +0200

    Sophie made changes to slider js

--- src/components/js/slider.js
+++ src/components/js/slider.js
@@ -1,3 +1,11 @@
-function submit(search) {
-
+function submit() {
+    let content = document.getElementById("sliderSearch").value;
+     document.getElementById("sliderSearch").value='';
+    
+    if (content === '' )  {
+        alert("Please enter a search text");
+    } else {
+        alert(content); 
+        content = "";
+    }
 }
